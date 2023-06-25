7f1095eb3537a79ba213021141a97093ea73b5a6
ritumahato <ritumahato592002@gmail.com>
2023-06-25 08:54:06 +0530

    changes

--- src/app/Modal.tsx
+++ src/app/Modal.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React, { useEffect, useState } from 'react'
 import { AiOutlineClose, AiTwotoneAppstore } from "react-icons/ai";
-import { AiFillDatabase, AiOutlineUsergroupDelete, AiOutlineVerticalAlignBottom, AiOutlineUndo, AiFillBulb, AiTwotoneBug, AiOutlineSave, AiTwotoneEdit } from "react-icons/ai";
+import { AiFillDatabase, AiOutlineUsergroupDelete, AiOutlineVerticalAlignBottom, AiOutlineUndo, AiFillBulb, AiTwotoneBug, AiOutlineSave, AiTwotoneEdit, AiFillCopyrightCircle, AiFillMessage } from "react-icons/ai";
 import ReactMarkdown from 'react-markdown';
 
 
@@ -9,6 +9,8 @@ const Modal = ({ isVisible, onClose, selectedItem }) => {
     if (!isVisible) return null;
     const [selectedDiv, setSelectedDiv] = useState(null);
     const [editableContent, setEditableContent] = useState('Markdown Supported here..');
+    const [editableNote, setEditableNote] = useState('');
+
     const [isEditing, setIsEditing] = useState(false);
 
     const handleButtonClick = (selected) => {
@@ -24,6 +26,9 @@ const Modal = ({ isVisible, onClose, selectedItem }) => {
     const handleInputChange = (event) => {
         setEditableContent(event.target.value);
     };
+    const handleEditNote = (event) => {
+        setEditableNote(event.target.value);
+    }
 
     const renderSelectedDiv = () => {
         console.log(selectedDiv)
@@ -55,12 +60,6 @@ const Modal = ({ isVisible, onClose, selectedItem }) => {
                             <ReactMarkdown>{editableContent}</ReactMarkdown>
                         </div>
                     )}
-                    {/* <div className='p-7 text-[15px] '>
-                        Are you passionate about UI/UX design and eager to gain hands-on experience working with a prestigious Management Consulting firm? TalentKompass Deutschland, a top Human Resources company based in Germany, is searching for a motivated UI/UX Design Intern to join our esteemed client. This remote position offers an extraordinary chance for someone who is excited to learn and develop their skills in a dynamic and fast-paced environment.
-
-                        As a UI/UX Design Intern, you will work closely with the consulting and design teams, where you will be responsible for a range of tasks, including user research, wireframing, prototyping, and usability testing. You will have the unique opportunity to learn from experienced professionals who will provide mentorship and guidance throughout the internship. With this internship, you will gain valuable experience in UI/UX design, design thinking, and teamwork – all essential skills for a successful career in this field.
-
-                    </div> */}
                 </div>
                     ;
             case 'div2':
@@ -80,17 +79,40 @@ const Modal = ({ isVisible, onClose, selectedItem }) => {
                         <div className="heading p-7 font-semibold text-xl">
                             Notes
                         </div>
-                        <div><button className='p-7' onClick={handleEditClick}><AiTwotoneEdit size={25} /></button></div></div>
+                        <div className='flex items-center'><button className='p-7 bg-blue-300 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-lg' onClick={handleEditClick}>Create New Note</button></div></div>
                     <div className="flex justify-center flex-col items-center">
-                        <div><AiOutlineSave size={300} color='#C9BCE3' /></div>
-                        <div>No notes created</div>
+                        {
+                            isEditing ? (
+                                <textarea value={editableNote} onChange={handleEditNote} className='h-[440px] w-[750px] p-[10px]' />
+                            ) : (editableNote === "" ? (<div><AiOutlineSave size={300} color='#C9BCE3' /><div>No notes created</div></div>) : (<div className='flex text-left '> {editableNote} </div>)
+                            )
+                        }
+                        {
+                            isEditing ? (<button className='p-7 bg-blue-300 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-lg' onClick={handleSaveClick}>Save</button>)
+                                : (
+                                    <div></div>
+                                )
+                        }
                     </div></div>;
             case 'div4':
                 return <div><div className="heading p-7 font-semibold text-xl">
                     Your network
                 </div>
                     <div className="abouthejob pl-7 text-lg">
-                        {/* About the Job */}
+                        <div className="max-w-sm w-full lg:max-w-full lg:flex shadow-lg">
+                            <div className="border-r border-b border-l  bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+                                <div className=" flex gap-16 items-center">
+                                    <AiFillCopyrightCircle size={70} color='#57A4F2' />
+                                    <div className='flex flex-col'>
+                                        <div className="text-gray-900 font-bold text-lg mb-2">Monisha Rout</div>
+                                        <div className='text-xs text-gray-400'>UI UX designer at Crisil</div>
+                                    </div>
+                                    <p className="text-gray-400 text-base">Name of the company</p>
+                                    <p className="text-gray-400 text-base">17 June, 2023</p>
+                                    <AiFillMessage color='#57A4F2' size={30}/>
+                                </div>
+                            </div>
+                        </div>
                     </div></div>;
             default:
                 return <div><div className="heading p-7 font-semibold text-xl">
@@ -125,20 +147,14 @@ const Modal = ({ isVisible, onClose, selectedItem }) => {
                     <div className="dashboard p-5">
                         <div className="sidebar"> <div className="menu">
                             <div className='bg-slate-50  w-56 sidebar grid grid-rows-4'>
-                                <div className='p-3.5 items-center flex gap-1 role1' onClick={() => handleButtonClick('div1')} style={{ backgroundColor: selectedDiv === 'div1' ? 'antiquewhite' : '' }}><AiFillDatabase size={30} />Job Description</div>
-                                <div className='p-3.5 items-center flex gap-1 role1' onClick={() => handleButtonClick('div2')} style={{ backgroundColor: selectedDiv === 'div2' ? 'antiquewhite' : '' }}><AiOutlineUsergroupDelete size={30} />Company</div>
-                                <div className='p-3.5 items-center flex gap-1 role1' onClick={() => handleButtonClick('div3')} style={{ backgroundColor: selectedDiv === 'div3' ? 'antiquewhite' : '' }}><AiOutlineVerticalAlignBottom size={30} />Note</div>
-                                <div className='p-3.5 items-center flex gap-1 role1' onClick={() => handleButtonClick('div4')} style={{ backgroundColor: selectedDiv === 'div4' ? 'antiquewhite' : '' }}><AiOutlineUndo size={30} />Networking</div>
+                                <div className='p-3.5 items-center flex gap-1 role1' onClick={() => handleButtonClick('div1')} style={{ backgroundColor: selectedDiv === 'div1' ? '#F7F3FF' : '' }}><AiFillDatabase size={30} />Job Description</div>
+                                <div className='p-3.5 items-center flex gap-1 role1' onClick={() => handleButtonClick('div2')} style={{ backgroundColor: selectedDiv === 'div2' ? '#F7F3FF' : '' }}><AiOutlineUsergroupDelete size={30} />Company</div>
+                                <div className='p-3.5 items-center flex gap-1 role1' onClick={() => handleButtonClick('div3')} style={{ backgroundColor: selectedDiv === 'div3' ? '#F7F3FF' : '' }}><AiOutlineVerticalAlignBottom size={30} />Note</div>
+                                <div className='p-3.5 items-center flex gap-1 role1' onClick={() => handleButtonClick('div4')} style={{ backgroundColor: selectedDiv === 'div4' ? '#F7F3FF' : '' }}><AiOutlineUndo size={30} />Networking</div>
                             </div>
                         </div></div>
                         <div className="contentOfSidebar w-[840px] h-[617px] bg-slate-100">
                             {renderSelectedDiv()}
-                            {/* <div className="heading p-7 font-semibold text-xl">
-                                Description
-                            </div>
-                            <div className="abouthejob pl-7 text-lg">
-                                About the Job
-                            </div> */}
                         </div>
                     </div>
 
